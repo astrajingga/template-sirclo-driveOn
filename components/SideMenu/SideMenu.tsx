2c2b3f9ce1a92bf29412376421aa0aab8ee76473
narukane <karomyfatahillah@gmail.com>
2021-11-09 14:08:12 +0800

    issue #6; Task 1 : font family header to poppins, Task 2 : font size 14px pada mobile dan font family poppins, Task 3 : Logo pada side menu, Task 4 : warna Icon Navigasi

--- components/SideMenu/SideMenu.tsx
+++ components/SideMenu/SideMenu.tsx
@@ -1,12 +1,29 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+/* library Package */
+import { Logo } from '@sirclo/nexus'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { LazyLoadComponent } from 'react-lazy-load-image-component'
 
 const SideMenu = ({ title, openSide, toogleSide, children, positionSide }) => {
   return (
     <>
-      <div className={openSide ? `side-menu fade show ${positionSide}` : `side-menu fade`}>
+      <div
+        className={
+          openSide ? `side-menu fade show ${positionSide}` : `side-menu fade`
+        }
+      >
         <div className="header-side-menu">
-          <h6>{title}</h6>
+          <LazyLoadComponent>
+            <Logo
+              imageClassName="nav-logo"
+              thumborSetting={{
+                width: 100,
+                format: "webp",
+                quality: 100,
+              }}
+           />
+          </LazyLoadComponent>
+          
           <FontAwesomeIcon
             className="icon"
             icon={faTimes}
@@ -16,9 +33,13 @@ const SideMenu = ({ title, openSide, toogleSide, children, positionSide }) => {
         <hr className="side-menu-hr" />
         {children}
       </div>
-      <div className="bg-outside" style={{ display: openSide ? 'block' : 'none' }} onClick={toogleSide}></div>
+      <div
+        className="bg-outside"
+        style={{ display: openSide ? "block" : "none" }}
+        onClick={toogleSide}
+      ></div>
     </>
-  )
-}
+  );
+};
 
 export default SideMenu;
